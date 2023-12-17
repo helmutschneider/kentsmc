74891f2b5e532ef43041971998b791d9093377eb
Johan Björk <johanimon@gmail.com>
2023-12-18 00:54:45 +0100

    Add value conversions for more SMC key types

--- KentSMC.swift
+++ KentSMC.swift
@@ -55,8 +55,13 @@ struct SMCString : Equatable {  // 4 bytes
         return String(chars)
     }
 
-    public static let typeUInt32 = SMCString("ui32")
-    public static let typeFloat32 = SMCString("flt ")
+    static let typeInt8 = SMCString("si8 ")
+    static let typeInt32 = SMCString("si32")
+    static let typeInt64 = SMCString("si64")
+    static let typeUInt8 = SMCString("ui8 ")
+    static let typeUInt32 = SMCString("ui32")
+    static let typeUInt64 = SMCString("ui64")
+    static let typeFloat32 = SMCString("flt ")
 }
 
 enum SMCOperation : UInt8 {
@@ -96,25 +101,61 @@ struct SMCEntry { // 4 + 6(+2) + 16 + 12 + 1 + 1 + 1(+1) + 4 + 32 = 80
 }
 
 enum SMCValue {
-    case unknown
-    case f32(Float)
+    case i8(Int8)
+    case i16(Int16)
+    case i32(Int32)
+    case i64(Int64)
+    case u8(UInt8)
+    case u16(UInt16)
     case u32(UInt32)
+    case u64(UInt64)
+    case f32(Float32)
+    case f64(Float64)
+    case unknown(String)
 
-    static func fromBytes(_ bytes: SMCBytes, _ type: SMCString) -> Self {
-        switch type {
+    static func fromBytes(_ bytes: SMCBytes, _ info: SMCInfo) -> Self {
+        let u32: UInt32 = (UInt32(bytes.0) << 24)
+            | (UInt32(bytes.1) << 16)
+            | (UInt32(bytes.2) << 8)
+            | (UInt32(bytes.3))
+
+        let u64: UInt64 = (UInt64(bytes.0) << 56)
+            | (UInt64(bytes.1) << 48)
+            | (UInt64(bytes.2) << 40)
+            | (UInt64(bytes.3) << 32)
+            | (UInt64(bytes.4) << 24)
+            | (UInt64(bytes.5) << 16)
+            | (UInt64(bytes.6) << 8)
+            | (UInt64(bytes.7))
+
+        switch info.type {
+            case SMCString.typeInt8:
+                let x = Int8(bytes.0)
+                return .i8(x)
+            case SMCString.typeInt32:
+                let x = Int32(u32)
+                return .i32(x)
+            case SMCString.typeInt64:
+                let x = Int64(u64)
+                return .i64(x)
+            case SMCString.typeUInt8:
+                return .u8(bytes.0)
             case SMCString.typeUInt32:
-                var x: UInt32 = (UInt32(bytes.0) << 24)
-                    | (UInt32(bytes.1) << 16)
-                    | (UInt32(bytes.2) << 8)
-                    | (UInt32(bytes.3));
-                return .u32(x)
-            default: return .unknown
+                return .u32(u32)
+            case SMCString.typeUInt64:
+                return .u64(u64)
+            case SMCString.typeFloat32:
+                let big = UInt32(bigEndian: u32)
+                let f32 = Float(bitPattern: big)
+                return .f32(f32)
+            default: return .unknown(info.type.toString())
         }
     }
 }
 
 enum SMCError : Error {
     case iokit(kern_return_t)
+    case string(String)
 }
 
 class SMCConnection {
@@ -161,20 +202,91 @@ class SMCConnection {
         input.data8 = SMCOperation.kSMCReadKey.rawValue
         input.info.size = outInfo.info.size
         let out = try callStructMethod(&input)
-        let v = SMCValue.fromBytes(out.bytes, outInfo.info.type)
+        let v = SMCValue.fromBytes(out.bytes, outInfo.info)
 
         return v
+    }
+
+    func getKeyFromIndex(_ index: Int) throws -> SMCString {
+        var input = SMCEntry(SMCString(0))
+        input.data8 = SMCOperation.kSMCGetKeyFromIndex.rawValue
+        input.data32 = UInt32(index)
+        let out = try callStructMethod(&input)
+        return out.key
     }
     
     func write(_ key: String, value: SMCValue) throws {
         // todo!
     }
+
+    func keys() throws -> [SMCString] {
+        let numKeys = try read("#KEY")
+        guard case let .u32(numKeys) = numKeys else {
+            throw SMCError.string("expected a u32")
+        }
+        var keys: [SMCString] = []
+        for k in 0..<numKeys {
+            do {
+                let key = try getKeyFromIndex(Int(k))
+                keys.append(key)
+            } catch {}
+        }
+        return keys
+    }
 }
 
-assert(MemoryLayout<SMCEntry>.size == 80)
-assert(SMCString("#KEY").value == 592135513)
-assert(SMCString("#KEY").toString() == "#KEY")
+do {
+    assert(MemoryLayout<SMCEntry>.size == 80)
+    assert(SMCString("#KEY").value == 592135513)
+    assert(SMCString("#KEY").toString() == "#KEY")
+}
+
+let USAGE: String = """
+Usage:
+  kentsmc -k [key]               Read a key
+  kentsmc -k [key] -w [value]    Write a key
+  kentsmc -l                     List all keys
+""";
+
+var args: [String: String] = [:]
+var index = 1
+
+while index < CommandLine.argc {
+    let arg = CommandLine.arguments[index]
+    switch arg {
+        case "-k": do {
+            args["-k"] = CommandLine.arguments[index + 1]
+            index += 2
+        }
+        case "-w": do {
+            args["-w"] = CommandLine.arguments[index + 1]
+            index += 2
+        }
+        case "-l": do {
+            args["-l"] = ""
+            index += 1
+        }
+        default:
+            break
+    }
+}
+
+if args.isEmpty {
+    print(USAGE)
+    exit(1)
+}
 
 let conn = try! SMCConnection()
-let numKeys = try! conn.read("#KEY")
-print(numKeys)
+
+if args.keys.contains("-l") {
+    let keys = try conn.keys()
+    for key in keys {
+        do {
+            let value = try conn.read(key.toString())
+            print("\(key.toString()) = \(value)")
+        } catch {}
+        
+    }
+}
+
+// let keys = try! conn.keys()
